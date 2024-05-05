2273c50ed1612c77f3c6875609fb31882687034b
mariano alvarez <mariano@codingco.io>
2024-05-04 21:26:24 -0300

    fix: try new title design on Us

--- src/app/components/Us.tsx
+++ src/app/components/Us.tsx
@@ -5,11 +5,12 @@ import Bg from "../../../public/home4.jpg";
 const Us: React.FC = () => {
   return (
     <div
-      className="flex flex-col px-10 lg:px-20 xl:px-40 py-20 gap-10 bg-gradient-to-b from-primary to-black border-t border-slate-200"
+      className="flex flex-col px-4 md:px-10 lg:px-20 xl:px-40 py-20 gap-10 bg-gradient-to-b from-primary to-black border-t border-slate-200"
       id="us"
     >
-      <div className="flex items-start justify-center col-span-1">
-        <h1 className="text-5xl text-slate-200 font-bold uppercase">
+      <div className="flex items-start justify-center col-span-1 relative">
+        <div className="absolute inline-block bg-secondary -skew-x-6 -skew-y-3 w-80 md:w-96 h-20 -top-5" />
+        <h1 className="text-3xl md:text-5xl text-slate-200 font-bold uppercase relative z-10 drop-shadow-lg">
           Nuestra Empresa
         </h1>
       </div>
